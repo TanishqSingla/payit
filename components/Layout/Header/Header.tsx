fa1413d85e725f24f25f255ee7c22f771394e2e0
TanishqSingla <tanishqsingla00@gmail.com>
2022-07-24 01:20:21 +0530

    Add tailwind
    Added tailwind and made a simple header with login button

---
+++ components/Layout/Header/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Header() {
+	return (
+		<div className="bg-background p-2 h-16 flex items-center">
+			<div className="text-3xl text-onBackground mx-2">
+				<Link href="/" passHref>
+					<a>Payit</a>
+				</Link>
+			</div>
+			<div className="grow"></div>
+			<nav>
+				<Link href="/login">
+					<button className="btn-primary m-8">Login</button>
+				</Link>
+			</nav>
+		</div>
+	);
+}
