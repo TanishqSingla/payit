efad002a999166aeda3d1f5870c4c7e1013339eb
Tanishq Singla <tanishqsingla00@gmail.com>
2022-10-15 23:16:04 +0530

    Add loading screen (#8)
    * Fix: redirect to home page for authenticated user
    
    * Fix: redirect logic
    
    * fix
    
    * Add loading screen
    
    * Fix: create payment width
    
    * fix: redirect logic in createPayment

--- pages/createPayment.tsx
+++ pages/createPayment.tsx
@@ -27,10 +27,8 @@ export default function CreatePayment() {
 	}, [uploadedFile]);
 
 	useEffect(() => {
-		if (!isUserAuthenticated()) {
-			router.push("/");
-		}
-	}, []);
+		isUserAuthenticated().catch(_ => router.replace('/'))
+	}, [router]);
 
 	const handleSubmit = (event: FormEvent) => {
 		event.preventDefault();
@@ -73,86 +71,84 @@ export default function CreatePayment() {
 				<title>Payit | Create Payement</title>
 				<meta name="description" content="Create payment"></meta>
 			</Head>
-			<form
-				onSubmit={handleSubmit}
-				className="max-w-2xl mx-auto rounded px-8 pt-6 pb-8 mb-4 space-y-4 surface"
-			>
-				<div>
-					<label htmlFor="payee">Payee</label>
-					<input
-						className="input"
-						name="payee"
-						required
-						placeholder="Payee"
-						onChange={(e) =>
-							setFormData({ ...formData, payee: e.target.value })
-						}
-						value={formData.payee}
-					/>
-				</div>
-				<div>
-					<label htmlFor="amount">Amount</label>
-					<input
-						className="input"
-						name="amount"
-						placeholder="Amount"
-						onChange={(e) =>
-							setFormData({
-								...formData,
-								amount: e.target.value.trim(),
-							})
-						}
-						value={formData.amount}
-					/>
-				</div>
-				<div>
-					<label htmlFor="fromEnterprise">From</label>
-					<select
-						className="input"
-						onChange={(e) =>
-							setFormData({
-								...formData,
-								fromEnterprise: e.target.value,
-							})
-						}
-						placeholder="From Enterprise?"
-						value={formData.fromEnterprise}
-					>
-						<option value="Tanishq Enterprises">
-							Tanishq Enterprises
-						</option>
-						<option value="Vinayak Enterprises">
-							Vinayak Enterprises
-						</option>
-						<option value="SS Enterprises">SS Enterprises</option>
-					</select>
-				</div>
-				<div>
-					<label htmlFor="status">Status:</label>
-					<select
-						className="input"
-						value={formData.status}
-						onChange={(e) =>
-							setFormData({ ...formData, status: e.target.value })
-						}
-					>
-						<option value="pending">Pending</option>
-						<option value="done">Done</option>
-						<option value="blocked">Blocked</option>
-					</select>
-				</div>
-				<div>
-					<label htmlFor="uploadImage">Upload File</label>
-					<input type="file" onChange={handleFileUpload} />
-				</div>
-				<Button
-					className="dark:bg-dark-secondary bg-primary"
-					type="submit"
-					loading={loading}
+			<main>
+				<form
+					onSubmit={handleSubmit}
+					className="max-w-2xl mx-auto rounded px-8 pt-6 pb-8 mb-4 space-y-4 surface"
 				>
-					Submit
-				</Button>
-			</form>
+					<div>
+						<label htmlFor="payee">Payee</label>
+						<input
+							className="input"
+							name="payee"
+							required
+							placeholder="Payee"
+							onChange={(e) =>
+								setFormData({ ...formData, payee: e.target.value })
+							}
+							value={formData.payee}
+						/>
+					</div>
+					<div>
+						<label htmlFor="amount">Amount</label>
+						<input
+							className="input"
+							name="amount"
+							placeholder="Amount"
+							onChange={(e) =>
+								setFormData({
+									...formData,
+									amount: e.target.value.trim(),
+								})
+							}
+							value={formData.amount}
+						/>
+					</div>
+					<div>
+						<label htmlFor="fromEnterprise">From</label>
+						<select
+							className="input"
+							onChange={(e) =>
+								setFormData({
+									...formData,
+									fromEnterprise: e.target.value,
+								})
+							}
+							placeholder="From Enterprise?"
+							value={formData.fromEnterprise}
+						>
+							<option value="Tanishq Enterprises">Tanishq Enterprises</option>
+							<option value="Vinayak Enterprises">Vinayak Enterprises</option>
+							<option value="SS Enterprises">SS Enterprises</option>
+						</select>
+					</div>
+					<div>
+						<label htmlFor="status">Status:</label>
+						<select
+							className="input"
+							value={formData.status}
+							onChange={(e) =>
+								setFormData({ ...formData, status: e.target.value })
+							}
+						>
+							<option value="pending">Pending</option>
+							<option value="done">Done</option>
+							<option value="blocked">Blocked</option>
+						</select>
+					</div>
+					<div>
+						<label htmlFor="uploadImage">Upload File</label>
+						<input type="file" onChange={handleFileUpload} />
+					</div>
+					<Button
+						className="dark:bg-dark-secondary bg-primary"
+						type="submit"
+						loading={loading}
+					>
+						Submit
+					</Button>
+				</form>
+			</main>
 		</>
 	);
 }
